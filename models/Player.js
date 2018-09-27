783f88369337e546f7e01f1a2a41e0b20de12003
Saung Park <spark89@gmail.com>
2018-09-27 15:50:07 -0400

    removed models folder

--- models/Player.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const playerSchema = new Schema({
-  playerName: String,
-  score: Number
-});
-
-mongoose.model('players', playerSchema);
