40e9a03a754ddf3c8ee53673242eef669d19cd16
Saung Park <spark89@gmail.com>
2018-10-16 16:40:07 -0400

    refactored tertiary operator

--- client/src/components/TimeHeader.js
+++ client/src/components/TimeHeader.js
@@ -1,34 +1,32 @@
 import React from 'react';
 import TimeLeft from './TimeLeft';
 
-export default ({ preRound, gameRound, isDrawer, seconds }) =>
-  preRound ? (
-    <div className="time-container">
-      {isDrawer ? (
+export default ({ preRound, gameRound, isDrawer, seconds }) => (
+  <div className="time-container">
+    {preRound ? (
+      isDrawer ? (
         <h4>Time left to become familiar with card: </h4>
       ) : (
         <h4>Pre-round ends: </h4>
-      )}
-      <TimeLeft seconds={seconds} />
-    </div>
-  ) : gameRound ? (
-    <div className="time-container">
+      )
+    ) : gameRound ? (
       <h4>Round ends: </h4>
-      <TimeLeft seconds={seconds} />
-    </div>
-  ) : null;
-  // preRound ? (
-  //   <div className="time-container">
-  //     {isDrawer ? (
-  //       <h4>Time left to become familiar with card: </h4>
-  //     ) : (
-  //       <h4>Pre-round ends: </h4>
-  //     )}
-  //     <TimeLeft seconds={seconds} />
-  //   </div>
-  // ) : gameRound ? (
-  //   <div className="time-container">
-  //     <h4>Round ends: </h4>
-  //     <TimeLeft seconds={seconds} />
-  //   </div>
-  // ) : null;
+    ) : null}
+    <TimeLeft seconds={seconds} />
+  </div>
+);
+// preRound ? (
+//   <div className="time-container">
+//     {isDrawer ? (
+//       <h4>Time left to become familiar with card: </h4>
+//     ) : (
+//       <h4>Pre-round ends: </h4>
+//     )}
+//     <TimeLeft seconds={seconds} />
+//   </div>
+// ) : gameRound ? (
+//   <div className="time-container">
+//     <h4>Round ends: </h4>
+//     <TimeLeft seconds={seconds} />
+//   </div>
+// ) : null;
